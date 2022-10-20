57e141a8cc613f3de5c5e6021ab24ff123befea3
faizaansheikh <princefaizan800@gmail.com>
2022-10-20 18:41:07 +0500

    accomplised

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,45 +2,83 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { MdDelete } from 'react-icons/md';
+import { AiFillEdit } from 'react-icons/ai';
+
 function Todo() {
     const [input, setinput] = useState("")
     const [items, setitems] = useState([])
-    const itemEvent  = (e)=>{
+    const [toggle, setToggle] = useState(true)
+    const[isEdit,setIsEdit] = useState(null)
+    const itemEvent = (e) => {
         setinput(e.target.value)
     }
-    const listItems = ()=>{
-        setitems((olditems)=>{
-          return[...olditems,input];
-        });
-        setinput("");
+    const listItems = () => {
+        if(!input){
+            alert("kuch likho to sai")
+        }
+        else if(input && !toggle){
+            setitems(
+                items.map((elem)=>{
+                    if(elem.id === isEdit){
+                        return{...elem, name:input}
+                    }
+                    return elem;
+                
+                })
+            )
+            setinput("")
+            setToggle(true)
+           
+        }else{
+            const allInputData = { id: new Date().getTime().toString(), name: input }
+            setitems([...items, allInputData])
+            setinput("");
+        }
     };
-    const deleteItems = (ind)=>{
-        items.splice(ind, 1);
-        setitems([...items]);
+    const deleteItems = (id) => {
+        // console.log(id);
+        const fin = items.filter((elem) => id !== elem.id)
+        setitems(fin);
     }
-    const deleteAll = ()=>{
+    const deleteAll = () => {
         setitems([]);
     }
-    
-  return (
-     <>
-   <div className='main'>
-       <h1>TODO LIST</h1>
-  <input type="text" placeholder='Enter todo' 
-  onChange={itemEvent}
-  value = {input}
-  />
-  <button className='btn' onClick={listItems}>+</button>
-  <button className='btntwo' onClick={deleteAll}>Delete All</button>
-  <ul>
-      {/* <li>{input}</li> */}
-      {items.map((itemval)=>{
-          return <li><span><MdDelete size={30} onClick={deleteItems}/></span>{itemval}</li>
-      })}
-  </ul>
-   </div>
-   </>
-  );
+    const editItem = (id) => {
+        const findId = items.find(elem => elem.id === id)
+        // console.log(findId);
+        setinput(findId.name)
+        setToggle(false)
+        setIsEdit(id)
+    }
+
+    return (
+        <>
+            <div className='main'>
+                <h1>TODO LIST</h1>
+                <input type="text" placeholder='Enter todo'
+                    onChange={itemEvent}
+                    value={input}
+                />
+              {
+                toggle?    <button className='btn' onClick={listItems}>+</button> : 
+                <button className='btn' onClick={listItems}><AiFillEdit size={25} /></button>
+              }
+              
+               
+                <button className='btntwo' onClick={deleteAll}>Delete All</button>
+                <ul>
+                    {/* <li>{input}</li> */}
+                    {items.map((elem) => {
+                        return <li>
+                            {elem.name}
+                            <span className='del'><MdDelete size={30} onClick={() => deleteItems(elem.id)} /></span>
+                            <span className='edi'><AiFillEdit size={30} onClick={() => editItem(elem.id)} /></span>
+                        </li>
+                    })}
+                </ul>
+            </div>
+        </>
+    );
 }
 
 export default Todo;
